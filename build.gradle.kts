8c43b3b39df1d38e3b2ad6ad7d09140f3d958e75
Asmit Mishra <260386875+yourasmit15-web@users.noreply.github.com>
2026-09-04 05:42:13 +0530

    chore: configure Android build plugins

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
+}
