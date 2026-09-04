f5b53d267ad12b17bce731c4d8cf73916d41a0a0
Asmit Mishra <260386875+yourasmit15-web@users.noreply.github.com>
2026-09-04 18:16:37 +0530

    fix: use Kotlin compilerOptions with AGP 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,12 @@ android {
     }
     buildTypes { release { isMinifyEnabled = true; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = "17" }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
